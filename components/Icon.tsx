3bb8a43628647e0330ac76fd3f43ac319a967b98
Isaac Hussain <isaaclearns58@gmail.com>
2025-08-21 09:26:47 +0300

    refactor: add default icon name, size and color

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import FontAwesome6 from "@expo/vector-icons/FontAwesome";
 
 export type TIconProps = {
   name: React.ComponentProps<typeof FontAwesome6>["name"];
-  color: React.ComponentProps<typeof FontAwesome6>["color"];
-  size: React.ComponentProps<typeof FontAwesome6>["size"];
+  color?: React.ComponentProps<typeof FontAwesome6>["color"];
+  size?: React.ComponentProps<typeof FontAwesome6>["size"];
 };
 
-export default function Icon(props: TIconProps) {
-  return <FontAwesome6 {...props} />;
+export default function Icon({
+  name = "home",
+  size = 24,
+  color = "#000000",
+}: TIconProps) {
+  return <FontAwesome6 name={name} color={color} size={size} />;
 }
