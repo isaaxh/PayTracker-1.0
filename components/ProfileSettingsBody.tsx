f8e643f1e954603be745729a2ba7f7db7c4db456
Isaac Hussain <isaaclearns58@gmail.com>
2025-08-28 16:36:19 +0300

    refactor: remove imports and unused consts

--- components/ProfileSettingsBody.tsx
+++ components/ProfileSettingsBody.tsx
@@ -1,15 +1,10 @@
 import { View } from "react-native";
 import React from "react";
-import { useGlobal } from "hooks/useGlobal";
-import { GlobalContextProps } from "@/services/providers/GlobalProvider";
-import { useColorScheme } from "nativewind";
 import ProfileSettingsForm from "./ProfileSettingsForm";
 
 const ProfileSettingsBody = () => {
-  const { colorScheme } = useColorScheme();
-  const { userData } = useGlobal() as GlobalContextProps;
   return (
-    <View className='mx-7 mt-3 flex-1'>
+    <View className='flex-1 mt-3 mx-7'>
       <ProfileSettingsForm />
     </View>
   );
