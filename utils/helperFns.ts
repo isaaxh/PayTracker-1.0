b30a9797c1cca8f2ab0944a7690d8b1e9ff905c9
Isaac Hussain <isaaclearns58@gmail.com>
2025-08-28 15:33:28 +0300

    add: add uibutton helper function

--- utils/helperFns.ts
+++ utils/helperFns.ts
@@ -1,3 +1,17 @@
+import { TVariantProps } from "@/components/ui/UIButton";
+
 export const capitalizeText = (text: string) => {
   return text.at(0)?.toUpperCase() + text.slice(1);
 };
+
+// helper functions for UIButton
+
+
+export const getTextColorKey = ({ textColor, variant, type }: TVariantProps) => {
+  if (textColor) return textColor;
+  if (variant === "link") return "link";
+  if (variant !== "fill" && type === "danger") return "danger";
+  if (variant !== "fill" && type === "success") return "success";
+  return "default";
+};
+
