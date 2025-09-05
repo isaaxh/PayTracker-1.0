a0ac5057de299bcbd01a9fa860da779c81f46a6d
Isaac Hussain <isaaclearns58@gmail.com>
2025-09-05 23:05:37 +0300

    add: add helper function to get daily totals

--- utils/currencyHelperFn.ts
+++ utils/currencyHelperFn.ts
@@ -1,3 +1,6 @@
+import { TTransaction } from "@/constants/TransactionsTypes";
+import { formatDate, getDayName } from "./dateHelperFn";
+
 type currencyConverterProps = {
   currency: string;
   rate: number;
@@ -12,4 +15,31 @@ export const convertCurrency = (props: currencyConverterProps) => {
   );
 
   return formattedAmount;
+};
+
+
+export type TDailyTotals = {
+  dayOfWeek: string;
+  totalAmount: number;
+};
+
+export const calculateDailyTotals = (
+  transactions: TTransaction[]
+): TDailyTotals[] => {
+  const weekdays = [0, 1, 2, 3, 4, 5, 6];
+
+  const dailySums: { [key: string]: number } = {};
+  transactions.forEach((transaction) => {
+    const dayOfWeek = formatDate(transaction.date, "day");
+    const amount = transaction.amount;
+
+    dailySums[dayOfWeek] = (dailySums[dayOfWeek] || 0) + amount;
+  });
+
+  const dailyTotalsArray: TDailyTotals[] = weekdays.map((dayOfWeek) => ({
+    dayOfWeek: getDayName(Number(dayOfWeek)),
+    totalAmount: dailySums[dayOfWeek] || 0,
+  }));
+
+  return dailyTotalsArray;
 };
