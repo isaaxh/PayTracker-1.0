6b423ed6bd8507586ab210c54bce1e7ef44c3588
Ishaq Hussain (Isaac) <isaaclearns58@gmail.com>
2025-09-30 22:49:29 +0300

    refactor: add guard

--- components/TransactionDetailsBody.tsx
+++ components/TransactionDetailsBody.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { router, useLocalSearchParams } from "expo-router";
 import { useDispatch } from "react-redux";
 
-import { useFetchUserData } from "@/hooks/useUserData";
 import { AppDispatch } from "@/services/state/store";
 import { removeTransaction } from "@/services/state/transactions/transactionSlice";
 import { useTransaction } from "@/hooks/useTransaction";
@@ -11,30 +10,31 @@ import { useTransaction } from "@/hooks/useTransaction";
 import TransactionDetailsList from "./TransactionDetailsList";
 import TransactionDetailsActions from "./TransactionDetailsActions";
 import TransactionDetailsSummaryCard from "./TransactionDetailsSummaryCard";
+import { useUserData } from "@/hooks/useUserData";
 
 const TransactionDetailsBody = () => {
   const { id } = useLocalSearchParams();
-  const { userData } = useFetchUserData();
+  const { userData } = useUserData();
 
   const { transaction, loading } = useTransaction(userData?.uid, id.toString());
 
   const dispatch = useDispatch<AppDispatch>();
 
   const onPressDelete = () => {
-    if (userData && transaction) {
-      dispatch(
-        removeTransaction({
-          transaction,
-          props: {
-            id: transaction.id,
-            collectionName: `users/${userData.uid}/transactions`,
-          },
-        })
-      );
-    } else {
+    if (!userData || !transaction) {
       console.log("No userData or transaction found.");
       return;
     }
+
+    dispatch(
+      removeTransaction({
+        transaction,
+        props: {
+          id: transaction.id,
+          collectionName: `users/${userData.uid}/transactions`,
+        },
+      })
+    );
 
     router.back();
   };
