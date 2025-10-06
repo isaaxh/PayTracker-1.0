e1058e497b9e8fa6849c7ac11fe482f316b5f299
Ishaq Hussain (Isaac) <isaaclearns58@gmail.com>
2025-10-06 08:32:20 +0300

    chore: disable serializableCheck

--- services/state/store.ts
+++ services/state/store.ts
@@ -23,9 +23,9 @@ const rootReducer = {
 
 export const store = configureStore({
     reducer: rootReducer,
-    // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-    //     serializableCheck: false
-    // }),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    }),
 })
 
 export const persistor = persistStore(store);
