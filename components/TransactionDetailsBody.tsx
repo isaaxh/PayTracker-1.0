9446841e5848c297c97ac2168d43f88beb385982
Ishaq Hussain (Isaac) <isaaclearns58@gmail.com>
2025-10-06 08:25:20 +0300

    feat: add removing and adding of transactions using react query

--- components/TransactionDetailsBody.tsx
+++ components/TransactionDetailsBody.tsx
@@ -1,40 +1,34 @@
 import { ScrollView, View } from "react-native";
 import React from "react";
 import { router, useLocalSearchParams } from "expo-router";
-import { useDispatch } from "react-redux";
 
-import { AppDispatch } from "@/services/state/store";
-import { removeTransaction } from "@/services/state/transactions/transactionSlice";
 import { useTransaction } from "@/hooks/useTransaction";
+import { useUserData } from "@/hooks/useUserData";
+import { useRemoveTransaction } from "@/hooks/useTransactions";
 
 import TransactionDetailsList from "./TransactionDetailsList";
 import TransactionDetailsActions from "./TransactionDetailsActions";
 import TransactionDetailsSummaryCard from "./TransactionDetailsSummaryCard";
-import { useUserData } from "@/hooks/useUserData";
 
 const TransactionDetailsBody = () => {
   const { id } = useLocalSearchParams();
   const { data: userData } = useUserData();
 
   const { transaction, loading } = useTransaction(userData?.uid, id.toString());
-
-  const dispatch = useDispatch<AppDispatch>();
+  const removeTransaction = userData?.uid
+    ? useRemoveTransaction(userData?.uid)
+    : null;
 
   const onPressDelete = () => {
-    if (!userData || !transaction) {
-      console.log("No userData or transaction found.");
+    if (!userData || !transaction || !removeTransaction) {
+      console.log("No userData or transaction or removeTransaction found.");
       return;
     }
 
-    dispatch(
-      removeTransaction({
-        transaction,
-        props: {
-          id: transaction.id,
-          collectionName: `users/${userData.uid}/transactions`,
-        },
-      })
-    );
+    removeTransaction.mutate({
+      id: transaction.id,
+      collectionName: `users/${userData.uid}/transactions`,
+    });
 
     router.back();
   };
--- hooks/useTransactions.ts
+++ hooks/useTransactions.ts
@@ -1,8 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
+import { useUserData } from '@/hooks/useUserData';
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Timestamp } from "firebase/firestore";
 
-import { TGetAllDocument } from './../services/api/firestoreApi';
+import { addDocument, removeDocument, TGetAllDocument, TRemoveDocument } from './../services/api/firestoreApi';
+
+import { firestoreTransactionSchema, TFirestoreTransaction, TTransaction } from "@/constants/TransactionsTypes";
 
 import { createTransactionQueryOptions } from "@/utils/queryOptions/createTransactionQueryOptions";
+
+import { ZodError } from "zod";
+
 
 export type UseTransactionProps = {
     uid: string;
@@ -16,7 +23,125 @@ export type TransactionFilterProps = {
     docLimit?: number | null;
 };
 
-export function useFetchTransactions({ uid, filter }: UseTransactionProps) {
-
+export function useFetchTransactions({ filter }: UseTransactionProps) {
+    const uid = useUserData().data?.uid ?? ''
     return useQuery(createTransactionQueryOptions({ uid, filter }, { enabled: !!uid }));
+}
+
+
+
+export function useAddTransaction() {
+    const { data: userData } = useUserData()
+    const uid = userData?.uid
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (newTransaction: TTransaction) => {
+
+            const firestoreReady = {
+                ...newTransaction,
+                date: Timestamp.fromDate(new Date(newTransaction.date)),
+            };
+            const parsedTransaction = firestoreTransactionSchema.parse(firestoreReady);
+
+            return addDocument<TFirestoreTransaction>({
+                id: newTransaction.id,
+                collectionName: `users/${uid}/transactions`,
+                data: parsedTransaction
+            })
+        },
+        onMutate: async (newTransaction) => {
+            // Cancel any outgoing refetches so they don’t overwrite optimistic update
+            await queryClient.cancelQueries({
+                queryKey: ['transactions', uid],
+            });
+
+            // Snapshot the previous value
+            const previousTransactions = queryClient.getQueryData<TTransaction[]>([
+                "transactions",
+                uid,
+            ]);
+
+            // Optimistically update cache
+            queryClient.setQueryData<TTransaction[]>(["transactions", uid], (oldTransactions) => [
+                ...(oldTransactions ?? []),
+                newTransaction,
+            ]);
+
+            // Return context for rollback if error
+            return { previousTransactions };
+        },
+        onError: (_err, _variables, context) => {
+            console.log('Adding transaction failed.');
+
+            if (!uid || !context?.previousTransactions) return;
+
+            if (_err instanceof ZodError) {
+                console.error("Validation error:", _err.errors);
+            } else {
+                console.error("Error adding transaction:", _err);
+            }
+
+            if (context?.previousTransactions)
+                queryClient.setQueryData(
+                    ['transactions', uid],
+                    context.previousTransactions
+                );
+        },
+        onSettled: () => {
+            if (!uid) return;
+
+            queryClient.invalidateQueries({
+                queryKey: ['transactions', uid],
+            });
+        },
+    })
+}
+
+
+export function useRemoveTransaction(uid: string, filter?: TransactionFilterProps) {
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: ({ id, collectionName }: TRemoveDocument) => {
+            return removeDocument({ id, collectionName })
+        },
+        onMutate: async ({ id }) => {
+            if (!uid) return;
+
+            // Cancel any outgoing refetches so they don’t overwrite optimistic update
+            await queryClient.cancelQueries({
+                queryKey: createTransactionQueryOptions({ uid, filter }).queryKey,
+            });
+
+            // Snapshot the previous value
+            const previousTransactions = queryClient.getQueryData<TTransaction[]>(
+                createTransactionQueryOptions({ uid, filter }).queryKey
+            );
+
+            // Optimistically update cache
+            queryClient.setQueryData<TTransaction[]>(
+                createTransactionQueryOptions({ uid, filter }).queryKey,
+                old => old ? old.filter(tx => tx.id !== id) : []
+            );
+
+            // Return context for rollback if error
+            return { previousTransactions };
+        },
+        onError: (_err, _variables, context) => {
+            if (!uid || !context?.previousTransactions) return;
+
+            queryClient.setQueryData(
+                createTransactionQueryOptions({ uid, filter }).queryKey,
+                context.previousTransactions
+            );
+        },
+        onSettled: () => {
+            if (!uid) return;
+
+            queryClient.invalidateQueries({
+                // queryKey: createTransactionQueryOptions({ uid, filter }).queryKey,
+                queryKey: ['transactions', uid],
+            });
+        },
+    })
 }
